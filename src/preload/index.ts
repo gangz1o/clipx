2ed509e6167866ab553ceccf5503808de202d776
Gangz1o <gangplankwong@gmail.com>
2025-03-10 18:21:39 +0800

    🦄 refactor: 删除冗余的代码

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -7,9 +7,6 @@ const api = {
   toggleSound: (soundEnabled: boolean) => ipcRenderer.invoke('toggleSound', soundEnabled)
 }
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
